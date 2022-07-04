3becad0accc62bc39109f3ab8b841a159adebd3b
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-07-04 13:23:53 +0500

    config file

--- data/config.py
+++ data/config.py
@@ -1,9 +1,18 @@
+from pathlib import Path
+
 from environs import Env
 
-# Теперь используем вместо библиотеки python-dotenv библиотеку environs
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = env.list("ADMINS")
+IP = env.str("ip")
+I18N_DOMAIN = 'mybot'
+
+BASE_DIR = Path(__file__).parent
+LOCALES_DIR = BASE_DIR / 'locales'
+
+LANGUAGES = ['uz', 'ru']
+
+LANG_STORAGE = {}
