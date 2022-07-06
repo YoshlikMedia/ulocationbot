b60330dea6def38f2a3607580df7b74a9ec5172c
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-07-06 18:25:34 +0500

    Get Phone Number Button

---
+++ keyboards/default/keyboard.py
@@ -0,0 +1,18 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+from data.texts import texts
+from middlewares import i18n
+
+_ = i18n.lazy_gettext
+
+get_number = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(
+                text=_(texts['get_phone_number']),
+                request_contact=True
+            )
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True)
