6d3bb413f52a134c96e2aa31d72450e2c9d508f0
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-07-07 23:03:22 +0500

    updated

--- utils/db_api/mongo.py
+++ utils/db_api/mongo.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from aiogram.contrib.fsm_storage.mongo import MongoStorage
 from data.config import IP
 
@@ -10,4 +11,3 @@ database = client['ulocationbot']
 LANG_STORAGE = database['lang_storage']
 USERS = database['users']
 CITY = database['city']
-
